080b1492c3f1dedf0824e4a9057de25c4c761571
somu616 <somujj@gmail.com>
2014-07-09 14:53:57 +0530

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Bhalla, Sumeet on 7/9/14.
+//  Copyright (c) 2014 Bhalla, Sumeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Bhalla, Sumeet on 7/9/14.
+//  Copyright (c) 2014 Bhalla, Sumeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
